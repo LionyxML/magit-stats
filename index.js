a5fa77cfcf424b8f7036957455ec5e06880f3307
LionyxML <rahul.juliato@gmail.com>
2023-01-22 00:49:48 -0300

    feat: implements basic git log to JSON decoding and commit count

---
+++ index.js
@@ -0,0 +1,24 @@
+import { exec } from "child_process";
+import prettier from "prettier";
+
+exec(
+  `git log --pretty=format:'{%n  "commit": "%H",%n  "abbreviated_commit": "%h",%n  "tree": "%T",%n  "abbreviated_tree": "%t",%n  "parent": "%P",%n  "abbreviated_parent": "%p",%n  "refs": "%D",%n  "encoding": "%e",%n  "subject": "%s",%n  "sanitized_subject_line": "%f",%n  "commit_notes": "%N",%n  "verification_flag": "%G?",%n  "signer": "%GS",%n  "signer_key": "%GK",%n  "author": {%n    "name": "%aN",%n    "email": "%aE",%n    "date": "%aD"%n  },%n  "commiter": {%n    "name": "%cN",%n    "email": "%cE",%n    "date": "%cD"%n  }%n},'`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+
+    const gitObject = JSON.parse(
+      prettier.format(`[${stdout}]`, { parser: "json" })
+    );
+
+    const commits = gitObject.length;
+
+    console.log({ commits });
+  }
+);
