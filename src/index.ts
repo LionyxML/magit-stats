15ae39e80640cfe7c406a2d0aceeee795ad151d5
LionyxML <rahul.juliato@gmail.com>
2023-01-30 23:46:27 -0300

    fix: fixes date ranges

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ const APP_DESC = "[magit-stats] - Your git repository statistics";
 const COMMAND = "npx magit-stats ";
 const CHECK_GIT_DIR_CMD = `git status`;
 const GIT_LOG_CMD = `git log --pretty=format:'{%n  "commit": "%H",%n  "abbreviated_commit": "%h",%n  "tree": "%T",%n  "abbreviated_tree": "%t",%n  "parent": "%P",%n  "abbreviated_parent": "%p",%n  "refs": "%D",%n  "encoding": "%e",%n  "sanitized_subject_line": "%f",%n  "commit_notes": "%N",%n  "verification_flag": "%G?",%n  "signer": "%GS",%n  "signer_key": "%GK",%n  "author": {%n    "name": "%aN",%n    "email": "%aE",%n    "date": "%aD"%n  },%n  "commiter": {%n    "name": "%cN",%n    "email": "%cE",%n    "date": "%cD"%n  }%n},'`;
-const DAY_HOURS = range(0, 23);
+const DAY_HOURS = range(0, 24);
 const WEEK_DAYS = range(0, 7);
 
 const logMsg = (msg: string | object) => console.log(msg);
